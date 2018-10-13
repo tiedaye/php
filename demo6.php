745a1b1bc601bf7c52ddbafff02219861d33703e
zhangshuang-h <781792980@qq.com>
2018-10-13 18:57:03 +0800

    demo6

---
+++ demo6.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/13
+ * Time: 16:25
+ */
+define("GREET","hello world!");
+//echo GREET;
+$a = "hello";
+$a .=" world";
+//echo $a;
+$b = 10;
+//echo $b--;
+//echo $b;
+$c = 4;
+$d = 4.00;
+echo var_dump($c!==$d);
+$e = 0;
+$f = 3;
+//echo var_dump($e and $f);

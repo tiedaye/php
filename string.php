c1f1acdb37c70828c819271310d14347c31eac0c
zhangshuang-h <781792980@qq.com>
2018-10-13 20:33:20 +0800

    字符串相关

---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/13
+ * Time: 16:18
+ */
+$str = "hello world!";
+echo strlen($str);
+$str1 = "hello dhu jui";
+echo var_dump(strpos("$str1","dhu"));

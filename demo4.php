2066ec7dfa6b125f433d41a8f50bf7756a45942e
zhangshuang-h <781792980@qq.com>
2018-10-13 15:24:40 +0800

    var_dump

---
+++ demo4.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/13
+ * Time: 14:38
+ */
+$d = "dddd";
+var_dump($d);
+echo "<br>";
+$e = 22;
+var_dump($e);
+echo "<br>";
+$f = true;
+var_dump($f);
+echo "<br>";
+$g = array("11","aaa",true);
+var_dump($g);

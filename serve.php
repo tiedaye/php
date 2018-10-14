108abc5e44240b77673706f1277ce86ed51235ec
zhangshuang-h <781792980@qq.com>
2018-10-14 11:32:49 +0800

    serve  跳转页面

---
+++ serve.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/14
+ * Time: 9:36
+ */
+$name = $_POST["username"];
+$pwd = $_POST["password"];
+//$sex = $_POST["sex"];
+//$hobby = $_POST["hobby"];
+//$type = $_POST["type"];
+//echo "用户名是" . $name . "密码是：" . $pwd,$sex;
+//echo "<br/>";
+//var_dump($hobby,$type);
+//if($name=='zhang' && $pwd == '222'){
+//    echo "登录成功";
+//}else{
+//    echo "用户名或密码错误";
+//}
